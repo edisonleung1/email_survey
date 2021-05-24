b5a1d562172f68de8623553f2f77adb46816c57d
Edison Liang <edisonliang@Edisons-MacBook-Pro.local>
2021-05-24 03:01:56 -0400

    fix env.var

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	googleClientID: '810358641543-na1sqq37spo686vjuq4fe5kkb4l2t93u.apps.googleusercontent.com',
-	googleClientSecret: 'FobhHtEWrPoJVwm05PYK3HXT',
-	mongoURI:'mongodb+srv://edisonleung1:MDlwg20160714@cluster0.d2e9i.mongodb.net/Cluster0?retryWrites=true&w=majority',
-	cookieKey:'asddsgdgsafsfsdfsdd',
-	stripePublishableKey:'pk_test_51IpH5uHRwuYsDYB5slTCjgDCpYKLL3A4RubKd5CMFo8cc3KFjVt4uUO8jDzOypofW0cRfZ4k7GFgJ7H4pYdUWaCR00VzOKev27',
-	stripeSecretKey:'sk_test_51IpH5uHRwuYsDYB5DE8eReJqzzgy2DG0n9It88GOirVZpOgrDFu96A2hG6lncmzhu37Z6trtgVRnjuMVuV2plNrH00nJuVmROC'
-};
