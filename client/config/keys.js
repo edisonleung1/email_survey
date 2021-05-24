885ca6dce09cb7e192f176249d820ae3d8a74902
Edison Liang <edisonliang@Edisons-MacBook-Pro.local>
2021-05-24 02:27:14 -0400

    add client react

---
+++ client/config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+	stripePublishableKey:"pk_test_51IpH5uHRwuYsDYB5slTCjgDCpYKLL3A4RubKd5CMFo8cc3KFjVt4uUO8jDzOypofW0cRfZ4k7GFgJ7H4pYdUWaCR00VzOKev27"
+};
+
+
+/*mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  */
