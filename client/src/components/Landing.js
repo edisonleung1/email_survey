339727ca374f697b817a746def1f7507277deb4f
Edison Liang <edisonliang@Edisons-MacBook-Pro.local>
2021-05-27 13:24:13 -0400

    remove_surveylink_on_landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,12 +4,7 @@ import {Link} from 'react-router-dom';
 const Landing=()=>{
   return(
     <div>
-      <h1>
-        Welcome to Wongchoy!
-      </h1>
-      <Link to="/surveys" className="green btn-flat white-text">
-        Surveys
-      </Link>
+      Landing!
     </div>
   );
 };
